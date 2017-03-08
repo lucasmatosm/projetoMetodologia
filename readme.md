51b26bc1c0f3a18e1a745721442aade8b3b3fd86
Lucas Matos de Medeiros <lucas.medeiros@ccc.ufcg.edu.br>
2017-03-07 23:09:28 -0300

    ajeitando o projeto

---
+++ readme.md
@@ -0,0 +1,3 @@
+Projeto de Metodologia
+
+Para rodar é so entrar na pasta src e rodar o script ./test.sh
---
+++ testa.sh
@@ -0,0 +1,16 @@
+cd src
+javac Simulator.java
+java Simulator first-fit 0.55 "Production"
+java Simulator first-fit 0.55 "Non-production"
+java Simulator best-fit 0.55 "Production"
+java Simulator best-fit 0.55 "Non-production"
+java Simulator worst-fit 0.55 "Production"
+java Simulator worst-fit 0.55 "Non-production"
+java Simulator best-fit 0.55 "Non-production"
+java Simulator first-fit 1 "Production"
+java Simulator first-fit 1 "Non-production"
+java Simulator best-fit 1 "Production"
+java Simulator best-fit 1 "Non-production"
+java Simulator worst-fit 1 "Production"
+java Simulator worst-fit 1 "Non-production"
+
